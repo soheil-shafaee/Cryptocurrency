d26b9ce3adc4a1effef9224005ef55edae99e58c
Soheil Shafaee <soheil.shafaee.6200@gmail.com>
2024-03-03 15:31:49 +0330

    ADD Main Class into Crypto_app file

--- Crypto_app.py
+++ Crypto_app.py
@@ -1,7 +1,9 @@
-from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel, QPushButton
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton
 from PyQt5.QtGui import QIcon
 from PyQt5 import uic
 import sys
+from loading import ProgressBar
+from main import Main
 
 
 class UI(QMainWindow):
@@ -14,12 +16,25 @@ class UI(QMainWindow):
         uic.loadUi("wellcome.ui", self)
 
         # Our Widgets
-        self.start_button = self.findChild(QPushButton, "")
+        self.start_button = self.findChild(QPushButton, "start_button")
+        self.load = None
+        self.main = None
+        self.start_button.clicked.connect(self.get_start)
 
         # Display Window
         self.show()
 
+    def get_start(self):
+        pass
+        # self.load = ProgressBar()
+        # self.load.show()
+        # self.main = Main()
+        # self.close()
+        # self.load.close()
+        # self.main.show()
 
-app = QApplication(sys.argv)
-Ui = UI()
-app.exec_()
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    Ui = UI()
+    sys.exit(app.exec_())
