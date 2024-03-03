c7c5f33abcddb61de0c99a33d502335adc9650b9
Soheil Shafaee <soheil.shafaee.6200@gmail.com>
2024-03-03 15:32:22 +0330

    ADD price and percent of the coin

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel, QComboBox
 from PyQt5 import uic
 import sys
+import requests
 
 
 class Main(QMainWindow):
@@ -13,6 +14,8 @@ class Main(QMainWindow):
         self.setFixedSize(398, 578)
 
         # Define Our Widgets
+        self.coin_price_list = []
+        self.coin_percent = []
         self.units_list = self.findChild(QComboBox, "units_list")
         self.unit1 = self.findChild(QLabel, "units1")
         self.btc_price = self.findChild(QLabel, "btc_price")
@@ -30,8 +33,67 @@ class Main(QMainWindow):
         self.usdt_price = self.findChild(QLabel, "usdt_price")
         self.usdt_percent = self.findChild(QLabel, "usdt_percent")
 
+        self.test = self.findChild(QLabel, "test")
+
+        self.units_list.addItems(["", "USD", "EUR"])
+        self.units_list.currentTextChanged.connect(self.update_units)
+        self.get_price()
         # Display Window
         self.show()
+
+    def update_units(self, text):
+        if text == "USD":
+            self.unit1.setText("$")
+            self.btc_price.setText(str(f"{self.coin_price_list[0]['USD']:,}"))
+            self.unit2.setText("$")
+            self.eth_price.setText(str(f"{self.coin_price_list[1]['USD']:,}"))
+            self.unit3.setText("$")
+            self.sol_price.setText(str(f"{self.coin_price_list[2]['USD']:,}"))
+            self.unit4.setText("$")
+            self.doge_price.setText(str(f"{self.coin_price_list[3]['USD']:,}"))
+            self.unit5.setText("$")
+            self.usdt_price.setText(str(f"{self.coin_price_list[4]['USD']:,}"))
+
+        elif text == "EUR":
+            self.unit1.setText("€")
+            self.btc_price.setText(str(f"{self.coin_price_list[0]['EUR']:,}"))
+            self.unit2.setText("€")
+            self.eth_price.setText(str(f"{self.coin_price_list[1]['EUR']:,}"))
+            self.unit3.setText("€")
+            self.sol_price.setText(str(f"{self.coin_price_list[2]['EUR']:,}"))
+            self.unit4.setText("€")
+            self.doge_price.setText(str(f"{self.coin_price_list[3]['EUR']:,}"))
+            self.unit5.setText("€")
+            self.usdt_price.setText(str(f"{self.coin_price_list[4]['EUR']:,}"))
+        if "-" in self.coin_percent[0]:
+            self.btc_percent.setStyleSheet("color: rgb(191, 1, 1)")
+        self.btc_percent.setText(self.coin_percent[0] + "%")
+        
+        if "-" in self.coin_percent[1]:
+            self.eth_percent.setStyleSheet("color: rgb(191, 1, 1)")
+        self.eth_percent.setText(self.coin_percent[1] + "%")
+
+        if "-" in self.coin_percent[2]:
+            self.sol_percent.setStyleSheet("color:  rgb(191, 1, 1)")
+        self.sol_percent.setText(self.coin_percent[2] + "%")
+
+        if "-" in self.coin_percent[3]:
+            self.doge_percent.setStyleSheet("color:  rgb(191, 1, 1)")
+        self.doge_percent.setText(self.coin_percent[3] + "%")
+
+        if "-" in self.coin_percent[4]:
+            self.usdt_percent.setStyleSheet("color:  rgb(191, 1, 1)")
+        self.usdt_percent.setText(self.coin_percent[4] + "%")
+
+    def get_price(self):
+        coin_list = ["BTC", "ETH", "SOL", "DOGE", "USDT"]
+        for coin in coin_list:
+            response = requests.get(f"https://min-api.cryptocompare.com/data/price?fsym={coin}&tsyms=USD,JPY,EUR")
+            response_percent = requests.get(
+                f"https://min-api.cryptocompare.com/data/pricemultifull?fsyms={coin}&tsyms=USD")
+            percent = response_percent.json()["DISPLAY"][f"{coin}"]["USD"]["CHANGEPCT24HOUR"]
+            self.coin_percent.append(percent)
+            self.coin_price_list.append(response.json())
 
 
 app = QApplication(sys.argv)
